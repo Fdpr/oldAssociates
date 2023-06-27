c4c0a234a2c149d0b9aff20b382759f30a16feaf
pablo <pablo@pablos-MacBook-Air-2.local>
2023-06-27 11:44:30 -0400

    updates

---
+++ testingONNX/testONNXModel.py
@@ -0,0 +1,56 @@
+import onnxruntime
+import torch
+import numpy as np
+import PIL
+batch_size = 1    # just a random number
+
+
+x = torch.randn(batch_size, 1, 224, 224, requires_grad=True)
+ort_session = onnxruntime.InferenceSession("./super_resolution.onnx")
+
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+# compute ONNX Runtime output prediction
+ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+ort_outs = ort_session.run(None, ort_inputs)
+
+# compare ONNX Runtime and PyTorch results
+# np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
+
+# print("Exported model has been tested with ONNXRuntime, and the result looks good!")
+
+
+from PIL import Image
+import torchvision.transforms as transforms
+
+img = Image.open("./catpicture.jpeg")
+
+resize = transforms.Resize([224, 224])
+img = resize(img)
+
+img_ycbcr = img.convert('YCbCr')
+img_y, img_cb, img_cr = img_ycbcr.split()
+
+to_tensor = transforms.ToTensor()
+img_y = to_tensor(img_y)
+img_y.unsqueeze_(0)
+
+
+ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(img_y)}
+ort_outs = ort_session.run(None, ort_inputs)
+img_out_y = ort_outs[0]
+
+
+img_out_y = Image.fromarray(np.uint8((img_out_y[0] * 255.0).clip(0, 255)[0]), mode='L')
+
+# get the output image follow post-processing step from PyTorch implementation
+final_img = Image.merge(
+    "YCbCr", [
+        img_out_y,
+        img_cb.resize(img_out_y.size, Image.BICUBIC),
+        img_cr.resize(img_out_y.size, Image.BICUBIC),
+    ]).convert("RGB")
+
+# Save the image, we will compare this with the output image from mobile device
+final_img.save("./cat_superres_with_ort.jpg")
